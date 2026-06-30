00d9ce9f5e051e63b56791bc5b26d33c6042463a
Maria Fornovi Gomez <mfornovi@c1r10s5.42madrid.com>
2026-06-30 11:31:55 +0200

    he devuelto el codigo a como estaba originalmente pues ya he entendido como usarlo sin modificar ese detalle

--- notmytalker.c
+++ notmytalker.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     }
 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // set to AF_INET to use IPv4
+    hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
     hints.ai_socktype = SOCK_DGRAM;
 
     rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo);
