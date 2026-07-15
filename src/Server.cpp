8a798515579cb986c1f5dbe49736dabe695e424b
Maria Fornovi Gomez <mfornovi@c1r9s4.42madrid.com>
2026-07-15 11:28:24 +0200

    Still trying to do this

--- src/Server.cpp
+++ src/Server.cpp
@@ -77,7 +77,7 @@ void Server::readClientInput(int fd)
 	if (nbytes <= 0)
 	{
 		if (nbytes == 0)
-			std::cout<<"Client %d hung up"<< cli.getFd()<<std::endl;
+			std::cout<<"Client "<< cli.getFd()<<" hung up"<<std::endl;
 		else
 			throw std::runtime_error("recv error: " + std::string(strerror(errno)));
 	}
@@ -116,12 +116,9 @@ void Server::pollLoop()
 						std::cerr << "accept error: " << strerror(errno) << std::endl;
 						return;
 					}
-					std::cout << "Viene otro!!" << std::endl;
 					Client client(client_fd);
 					this->_clients.insert(std::make_pair(client_fd, client));
 					_pfd_arr.push_back((pollfd){client_fd, POLLIN, 0});
-					std::cout<<client_fd<<std::endl;
-					std::cout<<client.getFd()<<std::endl<<std::endl;
 				}
 				else
 				{
@@ -129,7 +126,7 @@ void Server::pollLoop()
 					{
 						readClientInput(_pfd_arr[i].fd);
 						// datos de un cliente existente -> recv()
-						std::cout << "ESAMOS ESCRIBIENDO" << std::endl;
+						std::cout << "ESTAMOS ESCRIBIENDO" << std::endl;
 					}
 					catch(const std::exception& e)
 					{
@@ -139,7 +136,7 @@ void Server::pollLoop()
 					
 				}
 			}
-			else if (_pfd_arr[i].revents & (POLLERR | POLLHUP | POLLNVAL))
+			else if (_pfd_arr[i].revents & (POLLERR | POLLNVAL))
             {
                 std::cerr << "There was an error on socket " << _pfd_arr[i].fd << std::endl;
 				//desconectar y cerrar lo que corresponda
