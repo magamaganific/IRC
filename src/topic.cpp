562212463016527ed820579a971019d2c704273a
Maria Fornovi Gomez <mfornovi@c1r10s4.42madrid.com>
2026-08-13 16:31:24 +0200

    Trabajando el topic

--- src/topic.cpp
+++ src/topic.cpp
@@ -8,5 +8,10 @@
 
 // void cmdTopic(Server &s, Client &client, std::string line)
 // {
-
+// 	std::string channel = line.substr(0, line.find(" "));
+// 	std::string newtop;
+	
+// 	line = line.substr(line.find(" ") + 1, line.size());
+// 	if (line.find(":"))
+		
 // }
